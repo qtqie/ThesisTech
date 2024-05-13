58445550fb0b306ce00bc26809698e536921bed9
KianHe0620 <165177205+KianHe0620@users.noreply.github.com>
2024-05-13 10:47:50 +0800

    added login, register and implemented bootstrap

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,8 @@ Route::get('/', function () {
 
 Route::get('/login',[Login::class, 'LoginView']);
 Route::get('/addexpert',[ManageExpertProfile::class, 'addExpert']);
+Auth::routes();
+
+// Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
